63306740ed7bc8ef68dddb8e8204d251b62088d5
natepage <nathan.page86@gmail.com>
2019-06-05 20:45:32 +1000

    Make docker simpler

--- d.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Nathan updated that
-
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.local.yml "$@" || exit 1
--- dc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/bin/docker-compose -f docker-compose.yml -f docker-compose.local.yml -f docker-compose.dev.yml run --rm composer "$@" || exit 1
